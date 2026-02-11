57ffcc6cb1237dc90fca7359b4be40ae405a1054
niekdomi <dom.buehler@outlook.com>
2026-02-12 00:11:05 +0100

    cleanup

--- typetrace/frontend/main.cpp
+++ typetrace/frontend/main.cpp
@@ -1,18 +1,9 @@
 #include "application.hpp"
 
-#include <gio/gio.h>
+#include <gtkmm/application.h>
 
 auto main(int argc, char* argv[]) -> int
 {
-    // Register the GResource
-    auto* resource = typetrace_get_resource();
-    g_resources_register(resource);
-
-    auto app = typetrace::frontend::Application::create();
-    const int status = app->run(argc, argv);
-
-    // Unregister the GResource
-    g_resources_unregister(resource);
-
-    return status;
+    auto app = Gtk::Application::create("org.typetrace.frontend");
+    return app->make_window_and_run<typetrace::frontend::Application>(argc, argv);
 }
